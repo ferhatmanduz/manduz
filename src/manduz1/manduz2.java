7491fc915f64664755d2818c12c18f68af57f34f
ferhatm@gmail.com <ferhatmandz@gmail>
2021-08-07 01:40:04 -0400

    a

--- src/manduz1/manduz2.java
+++ src/manduz1/manduz2.java
@@ -7,13 +7,12 @@ public class manduz2 {
 		// ben bunu not ettim
 		System.out.println("b cdshgcgfh");
 		System.out.println("b1 cdshgcgfh");
-		// lololo
-<<<<<<< HEAD
+
 		System.out.println("2");
 		System.out.println("aaaa");
-=======
+
 		System.out.println();
->>>>>>> 093da47f3ac811c9fc0813f03ad2a0218141d8ae
+
 		
 	}
 
